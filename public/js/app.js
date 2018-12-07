d49620db4e855a08cbb22e24e43120803225cf6a
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-12-07 13:52:12 -0500

    able to remove players from state and scoreboard on click

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,7 @@ const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">
+      <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
         { props.name }
       </span>
 
@@ -79,6 +80,14 @@ class App extends React.Component {
     ]
   }
 
+  handleRemovePlayer = (id) => {
+    this.setState( prevState => {
+      return {
+        players: prevState.players.filter(person => person.id !== id)
+      }
+
+    })
+  }
 
   render(){
     return (
@@ -92,8 +101,9 @@ class App extends React.Component {
         {this.state.players.map( player =>
           <Player
             name={player.name}
-            score={player.score}
+            id={player.id}
             key={player.id.toString()}
+            removePlayer ={this.handleRemovePlayer}
           />
         )}
       </div>
